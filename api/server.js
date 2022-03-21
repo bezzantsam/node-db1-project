ff12e501fc51eab61d72a35bd5c4a5386aedce09
Sam Bezzant <sambezzant@Sams-MBP.home>
2022-03-21 16:20:33 -0700

    server

--- api/server.js
+++ api/server.js
@@ -4,4 +4,9 @@ const server = express();
 
 server.use(express.json());
 
+
+server.use('*', (req, res) => {
+    res.status(404).json({message: 'not found', error: 'not found'
+})
+})
 module.exports = server;
